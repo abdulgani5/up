16d6f609b9ed3f71b58735a0259d7073ab7cc089
abdulgani5 <321abdulgani@gmail.com>
2020-03-14 10:18:04 +0700

    up

---
+++ application/views/Home.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <title>.:: Latihan CodeIgniter ::.</title>
+    </head>
+    <body>
+       <h3>Hello, semuanya, selamat datang di CodeIgniter!</h3>
+    </html>
